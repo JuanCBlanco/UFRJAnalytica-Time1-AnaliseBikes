978272a3ab45f2a1b073478e165a0ff5b679dada
Juan Blanco <juanvcb@gmail.com>
2023-04-09 19:11:52 -0300

    Formatando corretamente

--- Exporacao 2023.1/Equipe 1/Scripts/Extracao.py
+++ Exporacao 2023.1/Equipe 1/Scripts/Extracao.py
@@ -1,18 +1,16 @@
 import pandas as pd
 
 
-
 def main():
     # ID do Google Drive
-    FILE_ID = '1a8gzQg37aaDHBO6OBVMieOZmNU7jTOL1'
-    
+    FILE_ID = "1a8gzQg37aaDHBO6OBVMieOZmNU7jTOL1"
+
     #  Link do arquivo
-    link = f'https://drive.google.com/uc?export=download&id={FILE_ID}'
+    link = f"https://drive.google.com/uc?export=download&id={FILE_ID}"
 
     # Obtendo o ID do arquivo
     pd.read_parquet(link).to_parquet("../Dados/BikeData.parquet", index=False)
-    
-    
-    
+
+
 if __name__ == "__main__":
     main()
