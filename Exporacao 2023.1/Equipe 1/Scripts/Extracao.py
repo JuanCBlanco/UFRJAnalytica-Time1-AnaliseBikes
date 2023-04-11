6b20c8dd4e64e1a74c15d60ec2cb212e310b94bf
Juan Blanco <juanvcb@gmail.com>
2023-04-11 15:11:22 -0300

    Realizando Tratamento dos Dados

--- Exporacao 2023.1/Equipe 1/Scripts/Extracao.py
+++ Exporacao 2023.1/Equipe 1/Scripts/Extracao.py
@@ -1,15 +1,21 @@
 import pandas as pd
+from Tratamento import tratamento_dados
 
-
-def main():
+def main() -> None:
     # ID do Google Drive
     FILE_ID = "1a8gzQg37aaDHBO6OBVMieOZmNU7jTOL1"
 
     #  Link do arquivo
     link = f"https://drive.google.com/uc?export=download&id={FILE_ID}"
 
-    # Obtendo o ID do arquivo
-    pd.read_parquet(link).to_parquet("../Dados/BikeData-Raw.parquet", index=False)
+    # Obtendo os Dados Brutos e Salvando na pasta Dados
+    df = pd.read_parquet(link)
+    df.to_parquet("../Dados/BikeData-Raw.parquet", index=False)
+    
+    #  Tratando os dados e salvando na pasta Dados
+    df = tratamento_dados(df)
+    df.to_parquet("../Dados/BikeData-Processed.parquet", index=False)
+    
 
 
 if __name__ == "__main__":
